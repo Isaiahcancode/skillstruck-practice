1164b1bf71a0f1157efe826141b3b9c1433d7277
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:13:07 -0700

    exported from Skill Struck

---
+++ python/mad lib.py
@@ -0,0 +1,12 @@
+
+noun1 = input("Choose a noun: ")
+pnoun1 = input("Choose a plural noun: ")
+noun2 = input("Choose a noun: ")
+place = input("Choose a place: ")
+adjective = input("Choose an adjective: ")
+
+print("Did you see the " + noun1 + "?")
+print("No, I went to see " + pnoun1 + ".")
+print("Is that where the " + noun2 + " is?")
+print("No, it's in " + place + ".")
+print("That sounds " + adjective + "!")
