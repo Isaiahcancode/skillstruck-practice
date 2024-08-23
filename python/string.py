69db7b2a7c55efe03adaffe860d0eacb62119656
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:13:02 -0700

    exported from Skill Struck

---
+++ python/string.py
@@ -0,0 +1,8 @@
+string1 = "i like to code."
+reverse = string1[len(string1)::-1]
+print(reverse)
+print(string1[4:9])
+print(len(string1))
+print(string1.rfind("i"))
+print(string1.split())
+print(string1[-3])
