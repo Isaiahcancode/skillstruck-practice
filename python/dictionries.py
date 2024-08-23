a195e7ef00d1a2f28cd245f648fcc0576077d274
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:13:57 -0700

    exported from Skill Struck

---
+++ python/dictionries.py
@@ -0,0 +1,10 @@
+mountains = {
+    "Timpanogos" : 2,
+    "Everest" : 1,
+    "Kilimanjaro" : 3,
+    "Vesuvius" : 4
+}
+
+mountains["Kilimanjaro"] = 5
+print(mountains)
+print(mountains["Vesuvius"])
