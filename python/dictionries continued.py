5ec348e68b318420f3d856cbd500fc596e6a1197
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:14:16 -0700

    exported from Skill Struck

---
+++ python/dictionries continued.py
@@ -0,0 +1,12 @@
+coins = {
+   		 "pennies" : 1,
+   		 "nickels": 5,
+  		 "dimes": 10,
+  		 "quarters": 25,
+}
+
+print(coins)
+coins["silver dollar"] = 100;
+coins.pop("pennies")
+print(coins)
+print(len(coins))
