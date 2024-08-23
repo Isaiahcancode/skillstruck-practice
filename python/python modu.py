4b4a33d7c778aea976a7e55e34687b1220a4b470
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:13:01 -0700

    exported from Skill Struck

---
+++ python/python modu.py
@@ -0,0 +1,8 @@
+string1 = "we like to code."
+print(string1.upper())
+string2 = "I can spell"
+print(string2.lower())
+string3 = "i can't read"
+print(string3.replace ("can't", "can"))
+string4 = 1
+print(string4.upper())
