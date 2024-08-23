ef5fc4b660c60bdf973e3abf4e73279aa8a55c36
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:13:08 -0700

    exported from Skill Struck

---
+++ python/loops if.py
@@ -0,0 +1,10 @@
+food = ["mushrooms", "broccoli", "fish"]
+for x in food:
+	if x == "mushrooms":
+		print(x + " are gross")
+	elif x == "broccoli":
+		print(x + "is gross")
+	elif x == "fish":
+		print(x + " is gross")
+	else:
+		print(x + "is not gross")
