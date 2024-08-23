456e53e3a0aa100d7afaae9bb8a7f151e3f6df5b
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:14:20 -0700

    exported from Skill Struck

---
+++ python/dictionaries looping chall 2.py
@@ -0,0 +1,16 @@
+first = int(input("Pick the height of box5"))		
+group = {
+	"box1" : 5,
+	"box2" : 2,
+	"box3" : 10,
+	"box4" : 3,
+	"box5" : first
+}
+
+total_volume = 0
+
+for x in group.values:
+	volume =  * 
+	total_volume =  + 
+
+print()
