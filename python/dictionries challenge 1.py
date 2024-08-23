436d920b124a5b3c0b0a27187007afbf6110a9f1
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:14:13 -0700

    exported from Skill Struck

---
+++ python/dictionries challenge 1.py
@@ -0,0 +1,14 @@
+holidays = {"January" : "New Years", 
+"july" : "forth",
+"august" : "wow",
+"we" : "who",
+"why" : "hehe",
+"stop" : "it", 
+"teach" : "teacher",
+"years" : "nine", 
+"haha" : "hoho",
+"night" : "day"
+}
+print(holidays)
+my_list = ["apples", "oranges", "grapes", "peaches", "blueberry"]
+print(my_list)
