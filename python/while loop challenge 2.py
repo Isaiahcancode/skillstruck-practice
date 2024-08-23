5ee1c88a69a90d4d052f088cbfc2f03ea0b1b5c1
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:14:07 -0700

    exported from Skill Struck

---
+++ python/while loop challenge 2.py
@@ -0,0 +1,11 @@
+counter = 0
+while True:
+   
+    user_input = int(input("Enter a number (0 to stop): "))
+
+    if user_input == 0:
+        print(counter)
+        break
+    else:
+        counter+= user_input
+    
