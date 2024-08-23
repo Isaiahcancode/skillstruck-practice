cf8d253a25cb1acdf014a03d750efa8af80ead21
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:13:52 -0700

    exported from Skill Struck

---
+++ python/number list.py
@@ -0,0 +1,12 @@
+lasers = [3, 10, 4, 15, 11,]
+
+print(lasers[2] * 10)
+pi = 3.14159265
+print(round(pi, 3))
+
+for x in range(0,10):
+    print(x)
+for y in range(0,10,2):
+    print(y)
+for z in range(10, 21, 10):
+    print(z)
