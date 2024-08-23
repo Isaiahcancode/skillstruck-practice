2f912b6831ba7757df066a67a7ce03ca079b2cf6
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:13:06 -0700

    exported from Skill Struck

---
+++ python/else statments.py
@@ -0,0 +1,9 @@
+coins = 10 
+
+if coins > 20:
+    print("you have more than enough to buy a puppy") 
+
+elif coins == 20:
+    print('you have exactly enough to buy a puppy')
+else:
+    print('you do not have enough to buy a puppy')
