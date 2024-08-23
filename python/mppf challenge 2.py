54bdf84b15d525dc3249d942c7ca32cc8811af99
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:14:12 -0700

    exported from Skill Struck

---
+++ python/mppf challenge 2.py
@@ -0,0 +1,11 @@
+choice1 = int(input("What is the first number?"))
+choice2 = int(input("What is the second number?"))
+
+def my_function(first, second):
+	if first < second :
+		print(choice1)
+	else:
+		print(choice2)
+		
+
+my_function(choice1,choice2 )
