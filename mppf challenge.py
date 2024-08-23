7c48714f2ec902f53b6f52a5800405d4a0ec46e0
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:14:11 -0700

    exported from Skill Struck

---
+++ mppf challenge.py
@@ -0,0 +1,8 @@
+fruit1 = input("Please enter the name of the fruit you would like: ")
+fruit2 = input("Please enter the name of the fruit you would like: ")
+fruit3 = input("Please enter the name of the fruit you would like: ")
+
+def hungryForApples(fruit1,fruit2,fruit3):
+    print(fruit1 + fruit2 + fruit3)
+
+hungryForApples(fruit1,fruit2,fruit3)
