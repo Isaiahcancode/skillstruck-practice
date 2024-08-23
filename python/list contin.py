e65ba008a52e37cf2334ae17f4b66380cb84a255
Isaiahcancode <ITayl715@evit.edu>
2024-08-23 08:13:53 -0700

    exported from Skill Struck

---
+++ python/list contin.py
@@ -0,0 +1,9 @@
+activities = ["movies", "skating", "bowling", "laser tag", "escape room", "trampoline park", "library"]
+
+print(activities[0:3])
+
+activities[8] = "water park"
+
+print(activities)
+
+print(len(activities))
